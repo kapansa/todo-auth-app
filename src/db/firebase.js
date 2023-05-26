4e19f41940809980f83385d2a60e2b202fa6bcbe
kapansa <samuelkapansa@gmail.com>
2023-05-26 14:09:37 +0200

    Add Components, db and pages folders

---
+++ src/db/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZrzdwwC_z-N1wLD5v8_6mJDAJJRuH8c8",
+  authDomain: "todo-auth-app-9f41d.firebaseapp.com",
+  projectId: "todo-auth-app-9f41d",
+  storageBucket: "todo-auth-app-9f41d.appspot.com",
+  messagingSenderId: "656519623429",
+  appId: "1:656519623429:web:55c03fce1be40c02adbd6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SignUp.css";
+
+export const SignUp = () => {
+  return <div>SignUp</div>;
+};
